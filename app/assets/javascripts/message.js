b387bbad81a20f0dfff5c64a299c14e5dae5fafc
kawajiro <59193739+kawajiro@users.noreply.github.com>
2020-01-18 10:01:13 +0900

    大変だ

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,77 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="one-message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="one-message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+
+$('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    console.log('STEP1');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+     .done(function(data){
+       console.log('success');
+       var html = buildHTML(data);
+       $('.messages').append(html);  
+       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});    
+       $('form')[0].reset();
+       $('.input__submit').prop('disabled',false);
+     })
+     .fail(function() {
+       console.log('fail');
+     })
+     .fail(function(){
+      alert('error');
+    })
+})
+});
+
+
+
+
+
+
---
+++ db/migrate/20200116074152_message.rb
@@ -0,0 +1,11 @@
+class Message < ActiveRecord::Migration[5.2]
+    def change
+      create_table :messages do |t|
+        t.string :content
+        t.string :image
+        t.references :group, foreign_key: true
+        t.references :user, foreign_key: true
+        t.timestamps
+      end
+    end
+  end
